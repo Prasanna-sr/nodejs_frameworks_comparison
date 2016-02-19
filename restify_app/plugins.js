afe0ecf5a883ec46eb351e1ac5b74c63e067df7a
prasanna ramanujam <prasannaraajkumar@gmail.com>
2016-02-18 16:24:12 -0800

    folder renamed

--- restify_app/plugins.js
+++ restify_app/plugins.js
@@ -1,7 +1,44 @@
 var restify = require('restify');
-var bunyan = require('bunyan');
 var server = restify.createServer();
+
+server.use(restify.conditionalRequest());
+
+
+/**
+* AuditLogger Plugin provides instrumentation
+**/
+var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: "myapp"});
+server.on('after', restify.auditLogger({
+    log: log
+}));
+
+/**
+* Serializers
+**/
+server.use(restify.requestLogger({
+    properties: {
+        foo: 'bar'
+    },
+    serializers: {
+
+    }
+}));
+
+/**
+* Throttle
+**/
+server.use(restify.throttle({
+  burst: 1,
+  rate: 1,
+  ip: true,
+  overrides: {
+    '192.168.1.1': {
+      rate: 0,
+      burst: 0
+    }
+  }
+}));
 
 server.use(function test(req, res, next) {
     console.log('ok');
@@ -11,6 +48,9 @@ server.use(function test(req, res, next) {
 server.get('/hello/:name', respond);
 
 function respond(req, res, next) {
+    var log = req.log;
+
+    log.error({params: req.params}, 'Hello there %s', 'foo');
     res.send('hello ' + req.params.name);
     privateFunction(req);
     next();
@@ -21,10 +61,6 @@ function privateFunction(req) {
     console.log('Private FF F');
     req.endHandlerTimer('privateFunction');
 }
-
-server.on('after', restify.auditLogger({
-    log: log
-}));
 
 server.listen(3000, function() {
     console.log('%s listening at %s', server.name, server.url);
